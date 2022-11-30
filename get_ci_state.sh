83446a920a17534c3c28fa303c3019efaf4dbc44
Jakob Fritz <j.fritz@fz-juelich.de>
2022-11-30 12:23:34 +0100

    Added comments in scripts

--- get_ci_state.sh
+++ get_ci_state.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 set -u
-##################################################################
 
+# Set a default waiting-time in seconds between queries,
+# if the pipeline finished
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
+# Get the id of the last pipeline that run for a given commit (GITHUB_SHA)
 pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${GITHUB_SHA}" | jq '.last_pipeline.id')
 
 echo "Triggered CI for branch ${GITHUB_REF}"
@@ -14,25 +16,32 @@ echo "Poll timeout set to ${POLL_TIMEOUT} s"
 
 ci_status="pending"
 
+# Repeat until the pipeline is neither pending nor running
 until [ "$ci_status" != "pending" ] && [ "$ci_status" != "running" ]
 do
+  # Wait some seconds
    sleep $POLL_TIMEOUT
+   # Get the current state of the pipeline and the url of the website
    ci_output=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}")
    ci_status=$(jq -n "$ci_output" | jq -r .status)
    ci_web_url=$(jq -n "$ci_output" | jq -r .web_url)
 
    echo "Current pipeline status: ${ci_status}"
+   # If the pipeline is running, print the current state (to end loop)
    if [ "$ci_status" = "running" ]
    then
      echo "Checking pipeline status..."
      curl -d '{"state":"pending", "target_url": "'${ci_web_url}'", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github.antiope-preview+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
    fi
 done
+# Pipeline is done
 
+# Show status of the pipeline
 echo "Pipeline finished with status ${ci_status}"
-
 echo "Fetching all GitLab pipeline jobs involved"
+# And get jobs that ran in this pipeline
 ci_jobs=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}/jobs" | jq -r '.[] | { id, name, stage }')
+# Give information on each job, but collapse this to not clutter the report
 echo "Posting output from all GitLab pipeline jobs"
 for JOB_ID in $(echo $ci_jobs | jq -r .id); do
   echo "##[group]Stage $( echo $ci_jobs | jq -r "select(.id=="$JOB_ID") | .stage" ) / Job $( echo $ci_jobs | jq -r "select(.id=="$JOB_ID") | .name" )"
@@ -40,8 +49,11 @@ for JOB_ID in $(echo $ci_jobs | jq -r .id); do
   echo "##[endgroup]"
 done
 echo "Debug problems by unfolding stages/jobs above"
+# Give the URL of the pipeline to make it easier to find and remove errors
 echo "For more details on the Jobs see: ${ci_web_url}"
 
+# Set result of the pipeline (success or failure) to own job (via curl)
+# and return that as a return-code (0 for success; 1 for failure)
 if [ "$ci_status" = "success" ]
 then
   curl -d '{"state":"success", "target_url": "'${ci_web_url}'", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
--- mirror.sh
+++ mirror.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 set -u
-##################################################################
 
+# Define what branches and tags, etc to fetch and to push
 branch="+refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/pull/*:refs/heads/pull/*"
 
+# Add the remote Gitlab-Repo to the local git
 echo "adding gitlab-repo as remote"
 git remote add gitlab "https://TOKENUSER:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
-sh -c "git fetch --force origin +refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/pull/*:refs/heads/pull/*"
+# Get the current state of the GitHub-Repo
+sh -c "git fetch --force origin $branch"
 
+# Push the current state of the repo to GitLab
 if [ "${FORCE_PUSH:-}" = "true" ]
 then
+  # Force push is used to make sure the gitlab-repo is the same as github
+  # If gitlab diverges, all changes from github are mirrored to GitLab
+  # even if that overwrites changes
   sh -c "git push --prune --force gitlab $branch"
 else
+  # If pushing without "force" creates merge-conflicts, the push is aborted
   sh -c "git push --prune gitlab $branch"
 fi
