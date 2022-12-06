1ef9e9610e320fe56c70244b0d5b537f632b805e
Jakob Fritz <j.fritz@fz-juelich.de>
2022-12-06 10:51:14 +0100

    Added echo to tell about syncing the token

--- copy_token.sh
+++ copy_token.sh
@@ -9,3 +9,5 @@ set -u
 variable_name="GITHUB_TOKEN"
 
 curl --request PUT --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/variables/${variable_name}" --form "value=${GITHUB_TOKEN}"
+echo "Done synchronizing the current Github-Token to Gitlab (used for later reporting of the pipeline-status)"
+echo ""
