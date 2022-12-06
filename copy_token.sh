fd1e9252b4df9acf55aae0658a999ea32d23e54e
Jakob Fritz <j.fritz@fz-juelich.de>
2022-12-06 13:45:52 +0100

    First try to create and later set status
    Creation is done in an early job in Github, setting it to "pending".
    Later, Gitlab shall set it to success or failure,
    depending on the result of the Gitlab-pipeline.

--- copy_token.sh
+++ copy_token.sh
@@ -8,6 +8,14 @@ set -u
 
 variable_name="GITHUB_TOKEN"
 
+# Copy the current token to gitlab
+# This token is used later to push the pipeline-result back to Github
 curl --request PUT --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/variables/${variable_name}" --form "value=${GITHUB_TOKEN}"
+
+# Create a status for the gitlab-pipeline and set the status to pending
+# This shall highlight in Github that another result is still missing.
+curl -d '{"state":"pending", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
+
+# Print something, so that it is clear in the logs, that this job ran.
 echo "Done synchronizing the current Github-Token to Gitlab (used for later reporting of the pipeline-status)"
 echo ""
