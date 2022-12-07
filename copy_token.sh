7fcc3e429f8dadd32d95ca5f32aeb0a95a18e396
Jakob Fritz <j.fritz@fz-juelich.de>
2022-12-07 12:33:54 +0100

    Switched to Check-Runs (first try)

--- copy_token.sh
+++ copy_token.sh
@@ -6,17 +6,27 @@
 
 set -u
 
-variable_name="GITHUB_TOKEN"
+# variable_name="GITHUB_TOKEN"
 
 # Copy the current token to gitlab
 # This token is used later to push the pipeline-result back to Github
-curl --request PUT --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/variables/${variable_name}" --form "value=${GITHUB_TOKEN}"
+# curl --request PUT --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/variables/${variable_name}" --form "value=${GITHUB_TOKEN}"
 
 # Create a status for the gitlab-pipeline and set the status to pending
 # This shall highlight in Github that another result is still missing.
-curl -d '{"state":"pending", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
+# curl -d '{"state":"pending", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
+
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITLAB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs \
+  -d '{"name":"Gitlab-CI","head_sha":"'"${GITHUB_SHA}"'","status":"in_progress"}'
+
 
 # Print something, so that it is clear in the logs, that this job ran.
-echo""
+echo ""
 echo "Done synchronizing the current Github-Token to Gitlab (used for later reporting of the pipeline-status)"
 echo ""
