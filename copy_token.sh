6d0d0e3bfa5f3b5a5d43281df183e51ee2532b37
Jakob Fritz <j.fritz@fz-juelich.de>
2022-12-07 12:37:36 +0100

    Use correct token

--- copy_token.sh
+++ copy_token.sh
@@ -20,7 +20,7 @@ set -u
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITLAB_TOKEN"\
+  -H "Authorization: token ${GITHUB_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs \
   -d '{"name":"Gitlab-CI","head_sha":"'"${GITHUB_SHA}"'","status":"in_progress"}'
