e53331c69eac6058994f8ee4749e236ac37d194d
Jakob Fritz <j.fritz@fz-juelich.de>
2022-12-09 15:39:15 +0100

    Added a closing of the check-run
    to avoid permanently open ones.

--- copy_token.sh
+++ copy_token.sh
@@ -16,17 +16,30 @@ set -u
 # This shall highlight in Github that another result is still missing.
 # curl -d '{"state":"pending", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null
 
-
-curl \
+check_reply=$(curl \
   -X POST \
+  --silent \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token ${GITHUB_TOKEN}"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs \
-  -d '{"name":"Gitlab-CI","head_sha":"'"${GITHUB_SHA}"'","status":"in_progress"}'
+  -d '{"name":"Gitlab-CI","head_sha":"'"${GITHUB_SHA}"'","status":"in_progress"}')
+check_id=$(jq -n "$check_reply" | jq -r .id)
+
+# Directly close it again, as otherwise it stays open and "in progress",
+# without any updates
+# and updates are not possible unless an allowed token is known.
+curl \
+  -X PATCH \
+  --silent \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/check-runs/${check_id} \
+  -d '{"name":"Gitlab-CI","head_sha":"'"${GITHUB_SHA}"'","status":"completed", "conclusion":"neutral"}'
 
 
 # Print something, so that it is clear in the logs, that this job ran.
 echo ""
-echo "Done synchronizing the current Github-Token to Gitlab (used for later reporting of the pipeline-status)"
+echo "Done creating and closing a check-run, that might be used for replies by Gitlab in a later stage of development."
 echo ""
