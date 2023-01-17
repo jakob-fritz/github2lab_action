34d56376eae0800a660bf5a383abf785af404f65
Jakob Fritz <j.fritz@fz-juelich.de>
2023-01-17 15:42:43 +0100

    adding many echos for debugging

--- get_artifact.sh
+++ get_artifact.sh
@@ -10,19 +10,23 @@ set -u -e
 pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${GITHUB_SHA}" | jq '.last_pipeline.id')
 jobs=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}/jobs?per_page=100")
 
-mkdir "artifacts"
-cd "artifacts"
+mkdir -p "artifacts"
+# cd "artifacts"
 
 # pagination is needed if pipeline has more than 100 jobs
+echo "starting to iterate through jobs"
+echo "$jobs"
 for job in $jobs
 do
+    echo "$job"
     if echo "$job" | jq --exit-status 'has("artifacts_file")' ; then
+        echo "found job with artifact"
         # Extract job-id and name of the job (to get the artifact later)
         job_id=$( echo "${job}" | jq '.id')
         job_name=$( echo "${job}" | jq '.name')
         echo "Job $job_id with name $job_name seems to have an artifact. Downloading it..."
         # Download artifact of this single job into file with the job-name
-        curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent --output "${job_name}.zip" "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/jobs/${job_id}/artifacts"
+        # curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent --output "${job_name}.zip" "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/jobs/${job_id}/artifacts"
         echo "Done dowloading the file from job $job_id $job_name"
     fi
 done
