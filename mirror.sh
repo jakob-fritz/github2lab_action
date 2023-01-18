cb4b87f9b9794e6c165fbffa21600e294fcf7785
jakob-fritz <37077134+jakob-fritz@users.noreply.github.com>
2023-01-18 16:59:35 +0100

    Solving Iss21 push single branch (#32)
    * Added fetch and pull for single branch
    
    Making prune obsolete, as only the single branch is pulled and pushed
    
    * Check out branch explicitly to avoid detached head
    
    * Removed git-statuses to avoid cluttering
    
    * Removed prune as variables
    
    because it is not used anymore
    (as only a single branch is pushed anyway)

--- mirror.sh
+++ mirror.sh
@@ -6,9 +6,6 @@
 
 set -u
 
-# Define what branches and tags, etc to fetch and to push
-branch="+refs/heads/*:refs/heads/* +refs/tags/*:refs/tags/* +refs/pull/*:refs/heads/pull/*"
-
 # Get the URL of the repo
 echo "Getting the URL of the repo"
 GITLAB_REPO_URL=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}" | jq '.http_url_to_repo' | sed -e 's/http:\/\///' -e 's/https:\/\///' -e 's/"//g')
@@ -17,33 +14,25 @@ echo "URL is https://${GITLAB_REPO_URL}"
 # Add the remote Gitlab-Repo to the local git
 echo "adding gitlab-repo as remote"
 git remote add gitlab "https://TOKENUSER:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
-# Get the current state of the GitHub-Repo
-sh -c "git fetch --force origin $branch"
-
+# Get current state of local git
+sh -c "git checkout ${GITHUB_REF_NAME}"
+# Get the current state of the single branch from the GitHub-Repo
+sh -c "git fetch --prune --no-tags --force origin ${GITHUB_REF_NAME}"
+sh -c "git pull --no-tags --force origin ${GITHUB_REF_NAME}"
+git status
 # Push the current state of the repo to GitLab
 if [ "${FORCE_PUSH:-}" = "true" ]; then
   # Force push is used to make sure the gitlab-repo is the same as github
   # If gitlab diverges, all changes from github are mirrored to GitLab
   # even if that overwrites changes
-  if [ "${PRUNE:-}" = "true" ]; then
-    # Check if the remote repo shall be pruned. Pruning leads to fewer obsolete
-    # and stale branches, but can lead to errors if multiple repos push there
-    sh -c "git push --prune --force gitlab $branch"
-  else
-    sh -c "git push --force gitlab $branch"
-  fi
+  sh -c "git push --force gitlab ${GITHUB_REF_NAME}"
 else
   # If pushing without "force" creates merge-conflicts, the push is aborted
-  if [ "${PRUNE:-}" = "true" ]; then
-    sh -c "git push --prune gitlab $branch"
-  else
-    sh -c "git push gitlab $branch"
-  fi
+  sh -c "git push gitlab ${GITHUB_REF_NAME}"
 fi
 # Get the return-code of pushing
 ret_code=$?
-if [ $ret_code = 0 ]
-then
+if [ $ret_code = 0 ]; then
   # Report, that pushing is done and add newline afterwards for formatting
   echo "Done pushing git-repo to https://${GITLAB_REPO_URL}"
 else
