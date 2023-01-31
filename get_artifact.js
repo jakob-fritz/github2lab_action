7828e801f695c299958507a1aecb3962f0048059
Jakob Fritz <j.fritz@fz-juelich.de>
2023-01-31 10:09:05 +0100

    Added function to download artifacts

--- get_artifact.js
+++ get_artifact.js
@@ -32,6 +32,51 @@ async function getListjobs() {
     return jobsObj;
 }
 
+async function downloadArtifacts(jobsPromise) {
+    console.log("Started downloadArtifact-function")
+    const jobs = await jobsPromise;
+    // console.log("Waited for jobs to be resolved")
+    // console.log(jobs)
+    for (let jobIdx = 0; jobIdx < jobs.length; jobIdx++) {
+        if (jobs[jobIdx].artifacts.some(element => element.file_type == "archive")) {
+            downloadSingleArtifact(jobs[jobIdx]);
+        } else {
+            console.log(`Job ${jobs[jobIdx].id} does not seem to exhibit an artifact`);
+        }
+    }      
+}
+
+
+async function downloadSingleArtifact(job) {
+    const fs = require('fs');
+    const {Readable} = require('stream');
+    const {finished} = require('stream/promises');
+
+    const getArtifact = singleJob => {
+        for (let idx = 0; idx < singleJob.artifacts.length; idx++){
+            if (singleJob.artifacts[idx].file_type == "archive") {
+                return singleJob.artifacts[idx]
+            }
+        }
+        throw `No artifact file found in job ${singleJob.id}`;
+    }
+    const artifact = getArtifact(job);
+    const fileName = artifact.filename
+    const jobName = job.name;
+    const jobID = job.id;
+    const fileURL = `https://${process.env.GITLAB_HOSTNAME}/api/v4/projects/${process.env.GITLAB_PROJECT_ID}/jobs/${jobID}/artifacts`;
+    console.log(`Job ${job.id} has an artifact with name ${fileName}`);
+    const fileStream = fs.createWriteStream(`${jobName}.${artifact.file_format}`);
+    const {body} = await fetch(fileURL, {
+        // headers: {
+        //     'PRIVATE-TOKEN': process.env.GITLAB_TOKEN,
+        // },
+        });
+    await finished(Readable.fromWeb(body).pipe(fileStream));
+    console.log(`Downloaded artifact from job ${jobName}`)
+}
 
 setEnvironmentVariables();
 const jobs = getListjobs();
+downloadArtifacts(jobs);
+// console.log(`Jobs that have been found: ${getListjobs()}`);
