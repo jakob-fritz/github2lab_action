47b053c8238d02eaee62d73c803f03d2a1ff0a66
Jakob Fritz <j.fritz@fz-juelich.de>
2024-01-03 10:01:52 +0100

    Rearranged git fetch and checkout
    Now using GITHUB_REF instead of GITHUB_REF_NAME,
    as this give a fully qualified name.
    
    It goes into detached head mode, but this should not pose an issue.
    
    First fetching before checkout to make sure, that the head is available (e.g. pull requests are not available otherwise)

--- mirror.sh
+++ mirror.sh
@@ -15,11 +15,11 @@ echo "URL is https://${GITLAB_REPO_URL}"
 echo "adding gitlab-repo as remote"
 git remote add gitlab "https://TOKENUSER:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
 # Get current state of local git
-echo "Running git checkout, git fetch, git pull"
-sh -c "git checkout ${GITHUB_REF_NAME}"
+echo "Running git fetch, git checkout, git pull"
 # Get the current state of the single branch from the GitHub-Repo
-sh -c "git fetch --prune --no-tags --force origin ${GITHUB_REF_NAME}"
-sh -c "git pull --no-tags --force origin ${GITHUB_REF_NAME}"
+sh -c "git fetch --prune --no-tags --force origin ${GITHUB_REF}"
+sh -c "git checkout ${GITHUB_REF}"
+sh -c "git pull --no-tags --force origin ${GITHUB_REF}"
 echo "git status is:"
 git status
 echo "Pushing to new gitlab remote"
