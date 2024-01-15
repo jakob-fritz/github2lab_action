a200816eb1bcfa3a00ea6bc86359331e7a9b0102
Jakob Fritz <j.fritz@fz-juelich.de>
2024-01-15 11:38:12 +0100

    Using env-var instead of github. -syntax

--- mirror.sh
+++ mirror.sh
@@ -18,8 +18,8 @@ git remote add gitlab "https://TOKENUSER:${GITLAB_TOKEN}@${GITLAB_REPO_URL}"
 echo "Running git fetch, git checkout, git pull"
 # Get the current state of the single branch from the GitHub-Repo
 # In case this is a pull-request, add this to fetch
-echo "Event is ${github.event_name}"
-if [ "${github.event_name}" = 'pull_request' ] || [ "${github.event_name}" = 'pull_request_target' ]
+echo "Event is ${GITHUB_EVENT_NAME}"
+if [ "${GITHUB_EVENT_NAME}" = 'pull_request' ] || [ "${GITHUB_EVENT_NAME}" = 'pull_request_target' ]
   then
     sh -c "git fetch --prune --no-tags --force origin +${GITHUB_REF}:${GITHUB_REF}"
   else
